cf5bc3c333f0e6a2cf23ffaa1051f9cc6da4015f
Equinox984 <lopezkris984@proton.me>
2026-08-12 12:34:24 -0600

    Initialize project with data import and variables
    Added imports and initialized variables for the physics project.

--- Main_Project/main.py
+++ Main_Project/main.py
@@ -1,1 +1,9 @@
+"""Physics Project"""
 
+# Import Packages
+import pandas as pd
+import numpy as np
+
+# Variables
+theta_max = 5
+data1_method = pd.read_csv("table1.txt", sep=r "\s+") # Create the Datafrane
