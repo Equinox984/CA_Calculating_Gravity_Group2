4be285f9b81b6b8f0ea1f99321cdb41706fbf2e0
Equinox984 <lopezkris984@proton.me>
2026-08-12 12:35:58 -0600

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-"""Physics Project"""
-
-# Import Packages
-import pandas as pd
-import numpy as np
-
-# Variables
-theta_max = 5
-data1_method = pd.read_csv("table1.txt", sep=r "\s+") # Create the Datafrane
