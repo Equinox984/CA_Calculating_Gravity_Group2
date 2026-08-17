a6ce4dd6cf4770ea3969ff98d07a817ec8714c21
Equinox984 <lopezkris984@proton.me>
2026-08-17 07:28:47 -0600

    Refactor DataFrame initialization and comment print
    Updated DataFrame creation and commented out print statement.

--- Main_Project/main.py
+++ Main_Project/main.py
@@ -7,5 +7,5 @@ import numpy as np
 # Variables
 theta_max = 5
 data1_method = pd.read_csv("table1.txt", sep=r"\s+") # Create the Datafrane
-
-print(data1_method)
+df1 = pd.DataFrame(data1_method, columns = ["theta_max", "10", "delta_10", "Avr_10", "sigma_10", "sigma_Avr_10"])
+# print(data1_method)
