9f0189728c320df0526e9747fe8bed4dcb6a0b53
Equinox984 <lopezkris984@proton.me>
2026-08-18 08:24:12 -0600

    Modify DataFrame structure and add data_analysis1 function
    Updated DataFrame columns and added data_analysis1 function to compute average.

--- Main_Project/main.py
+++ Main_Project/main.py
@@ -7,6 +7,16 @@ import numpy as np
 # Variables
 theta_max = 5
 data1_method = pd.read_csv("table1.txt", sep=r"\s+") # Create the Datafrane
-df1 = pd.DataFrame(data1_method, columns = ["theta_max", "10", "delta_10", "Avr_10", "sigma_10", "sigma_Avr_10"])
-print(df1)
+df1 = pd.DataFrame(data1_method, columns = ["theta_max", "L0", "delta_L0", "Avr_L0", "sigma_L0", "sigma_Avr_L0"])
+
+def data_analysis1(df1):
+    middle = len(df1)//2
+    average_lo = np.mean(df1["L0"])
+    df1.loc[middle, "Av1_L0"] = average_lo
+    return df1
+
+print(data_analysis1(df1))
+
+
+# print(df1)
 # print(data1_method)
